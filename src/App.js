08ab61df21b7f0d3ae94d575ebe3a52006e4c5e7
Mynul Islam <sk470004@gmail.com>
2022-08-19 01:14:59 +0600

    finished the project baseline

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import { useEffect, useState } from 'react';
 import './App.css';
+import RAppBar from './Pages/AppBar/RAppBar';
 import Home from './Pages/Home/Home';
 import Login from './Pages/Login/Login';
-import Navigation from './Pages/Nav Bar/Navigation';
+
 
 function App() {
 
 
   return (
     <div className="App">
-      {/* <Home /> */}
-      <Navigation />
-      <Login />
+
+      <RAppBar />
+      <Home />
+      {/* <Login /> */}
     </div>
   );
 }
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,34 +1,112 @@
 import { useEffect, useState } from 'react';
-
+import Accordion from '@mui/material/Accordion';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import Typography from '@mui/material/Typography';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material';
 
 function Home() {
     const [data, setData] = useState([]);
-    const [currentClass, setCurrentClass] = useState('52')
+    const [currentClass, setCurrentClass] = useState('49');
+    const [expanded, setExpanded] = useState(false);
+
+    const handleChange =
+        (panel) => (event, isExpanded) => {
+            setExpanded(isExpanded ? panel : false);
+        };
+
+
     useEffect(() => {
         fetch('http://localhost:5000/getroutine')
             .then(res => res.json())
             .then(data => {
                 let newDataArray = [];
-                let currentDay = ''
+                let currentDay = 'SAT'
+                let currentRoom = ''
                 let newDataObJ = {};
                 data.data.map((dtOBJ) => {
-                    if (dtOBJ.Day && dtOBJ.Room) {
-                        currentDay = dtOBJ.Day;
-                        newDataObJ = { ...dtOBJ }
-                        newDataArray.push(newDataObJ);
-                        newDataObJ = {};
-                    } else if (!dtOBJ.Day && dtOBJ.Room) {
-                        newDataObJ = { Day: currentDay, ...dtOBJ };
-                        newDataArray.push(newDataObJ);
-                        newDataObJ = {};
-                    }
+                    currentRoom = dtOBJ.Room;
+
+                    Object.keys(dtOBJ).map(function (key, index) {
+                        const value = dtOBJ[key];
+                        // SAT!=SUN
+                        if ((dtOBJ.Day) && (currentDay !== dtOBJ.Day)) {
+                            newDataObJ['Day'] = currentDay;
+                            const sortedObjectByKey = Object.entries(newDataObJ)
+                                .sort(([, a], [, b]) => {
+                                    let firstChar = a.charAt(0)
+                                    let secondChar = b.charAt(0)
+                                    return (firstChar - secondChar)
+                                })
+                                .reduce((r, [k, v]) => ({ ...r, [k]: v }), {});
+                            newDataArray.push(sortedObjectByKey);
+
+                            newDataObJ = {};
+                            currentDay = dtOBJ.Day;
+                        }
+                        else {
+                            if (value.includes(currentClass)) {
+                                if (key === "9:00-10:00") {
+                                    newDataObJ[key] = `1--${value}--${currentRoom}`;
+                                    // newDataObJ[`1--${key}`] = `${value}--${currentRoom}`;
+                                } else if (key === "10:00-11:00") {
+                                    newDataObJ[key] = `2--${value}--${currentRoom}`;
+                                    // newDataObJ[`2--${key}`] = `${value}--${currentRoom}`;
+
+                                }
+                                else if (key === "11:00-12:00") {
+                                    newDataObJ[key] = `3--${value}--${currentRoom}`;
+                                    // newDataObJ[`3--${key}`] = `${value}--${currentRoom}`;
+
+                                }
+                                else if (key === "12:00-1:00") {
+                                    newDataObJ[key] = `4--${value}--${currentRoom}`;
+                                    // newDataObJ[`4--${key}`] = `${value}--${currentRoom}`;
+
+                                }
+                                else if (key === "1:00-2:00") {
+                                    newDataObJ[key] = `5--${value}--${currentRoom}`;
+                                    // newDataObJ[`5--${key}`] = `${value}--${currentRoom}`;
+
+                                }
+                                else if (key === "2:00-3:00") {
+                                    newDataObJ[key] = `6--${value}--${currentRoom}`;
+                                    // newDataObJ[`6--${key}`] = `${value}--${currentRoom}`;
+
+                                }
+                                else if (key === "3:00-4:00") {
+                                    newDataObJ[key] = `7--${value}--${currentRoom}`;
+                                    // newDataObJ[`7--${key}`] = `${value}--${currentRoom}`;
+
+                                }
+                                else if (key === "4:00-5:00") {
+                                    newDataObJ[key] = `8--${value}--${currentRoom}`;
+                                    // newDataObJ[`8--${key}`] = `${value}--${currentRoom}`;
+                                }
+                            }
+                        }
+                    })
                 })
+                // push last element;
+                newDataObJ['Day'] = currentDay;
+                const sortedObjectByKey = Object.entries(newDataObJ)
+                    .sort(([, a], [, b]) => {
+                        let firstChar = a.charAt(0)
+                        let secondChar = b.charAt(0)
+                        return (firstChar - secondChar)
+                    })
+                    .reduce((r, [k, v]) => ({ ...r, [k]: v }), {});
+                newDataArray.push(sortedObjectByKey);
+                // filter the empty Day
+                newDataArray = newDataArray.filter(element => Object.keys(element).length > 1)
+
                 // let dayObj = {};
                 // currentDay = newDataArray[0].Day;
                 // let currentDayArray = [];
                 // newDataArray.map(NDA => {
                 //   if (NDA.Day === currentDay) {
-                //     currentDayArray.push(NDA);
+                //     cu<rrentDayArray.push(NDA);
                 //   } else {
                 //     // dayObj = { ...dayObj, currentDay: currentDayArray }
                 //     dayObj[currentDay] = currentDayArray;
@@ -40,26 +118,87 @@ function Home() {
                 // 
                 // console.log(newDataArray);
                 // console.log(dayObj);
+                // setData(data.data)
+
+
                 setData(newDataArray)
             });
     }, [])
+    // console.log('new ', data[2])
     return (
-        <div>
-            <h1>This is your home</h1>
-            {
-                data.map(dtObj => {
-                    return (
-                        Object.keys(dtObj).map(function (key, index) {
-                            const value = dtObj[key];
+        <div style={{ display: 'flex', justifyContent: 'center' }}>
+            <div>
+                <h1>This is your home</h1>
+                {
+                    data.map(dtObj => {
 
-                            return (
-                                value.includes(currentClass) && <li key={index}>{dtObj.Room}---{dtObj.Day}---{value}</li>
-                            )
+                        return (
+                            <div >
+                                <Accordion expanded={expanded === dtObj.Day} onChange={handleChange(dtObj.Day)} key={dtObj.Day}>
+                                    <AccordionSummary
+                                        expandIcon={<ExpandMoreIcon />}
+                                        aria-controls="panel4bh-content"
+                                        id="panel4bh-header"
+                                    >
+                                        <Typography sx={{ flexShrink: 0, fontWeight: 'bold', color: 'red' }}>{dtObj.Day}DAY</Typography>
+                                    </AccordionSummary>
+                                    <AccordionDetails>
 
-                        })
-                    )
-                })
-            }
+                                        <TableContainer sx={{ maxWidth: '650px', display: 'flex', alignItems: 'center', justifyContent: 'center' }} component={Paper}>
+                                            <Table sx={{}} aria-label="Appointments table">
+                                                <TableHead>
+                                                    <TableRow style={{ backgroundColor: '#95deed' }}>
+
+                                                        <TableCell >TIME</TableCell>
+                                                        <TableCell >SUBJECT</TableCell>
+                                                        <TableCell >Room</TableCell>
+
+                                                    </TableRow>
+                                                </TableHead>
+                                                <TableBody>
+
+
+
+                                                    {Object.keys(dtObj).map(function (key, index) {
+                                                        const value = dtObj[key];
+                                                        const valuesArray = value.split("--");
+
+
+                                                        return (
+                                                            value.includes(currentClass) &&
+                                                            <TableRow
+
+                                                                key={index}
+                                                                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                                                            >
+
+                                                                <TableCell component="th" scope="row">
+                                                                    {key}
+                                                                </TableCell>
+                                                                <TableCell component="th" scope="row">
+                                                                    {valuesArray[1]}
+                                                                </TableCell>
+                                                                <TableCell component="th" scope="row">
+                                                                    {valuesArray[2]}
+                                                                </TableCell>
+
+
+                                                            </TableRow>
+                                                        )
+
+                                                    })
+                                                    }
+                                                </TableBody>
+                                            </Table>
+                                        </TableContainer>
+
+                                    </AccordionDetails>
+                                </Accordion>
+
+                            </div>)
+                    })
+                }
+            </div>
         </div>
     );
 }
